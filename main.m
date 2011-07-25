eb45a68cfead033b2e469006e27bdeadde1b919d
Sheyne <me@sheyne.com>
2011-07-25 11:37:29 -0600

    First Commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TNC_TCP_Server
+//
+//  Created by Sheyne Anderson on 7/8/11.
+//  Copyright 2011 Sheyne Anderson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
